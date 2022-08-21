04459a5d6ace91080206ed28021b7ad75faa7342
ChristofferBillman <christoffer.billman@gmail.com>
2022-08-21 23:34:37 +0200

    feat: getting data from api now working also implemented a function getMockData to emulate api fetch.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
 import './styles/App.css'
 import './styles/Type.css'
 
+import { useState, useEffect } from 'react'
+
 import DataPoint from './components/DataPoint'
+
+import useFetch from './hooks/useFetch'
 
 import Thermostat from './icons/thermostat.svg'
 import Humidity from './icons/humidity.svg'
 import Waterdrop from './icons/waterdrop.svg'
 import Wind from './icons/wind.svg'
+
+import * as MockData from './MockData.json'
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const APIKEY = '1a87f44fa0405437594d18b5815bcaa8'
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const UMEA = '63.826867812391846, 20.263559761339305'
 
 const DataPointProps = [
 	{
@@ -31,6 +42,9 @@ const DataPointProps = [
 ]
 
 export default function App() {
+
+	const [weatherData, loading, error] = getMockData()
+
 	return (
 		<div className='App'>
 			<div className='app-container'>
@@ -42,8 +56,48 @@ export default function App() {
 				</div>
 				<div className='detail-container'>
 					<h1>Detail</h1>
+					<code>
+						{loading ? ('Loading...') :
+							error ? 'Something went wrong.' :
+								JSON.stringify(weatherData, null, 4)
+						}
+					</code>
 				</div>
 			</div>
 		</div>
 	)
+}
+
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function getWeatherData(lat: string, lon: string, apikey: string) {
+	// eslint-disable-next-line quotes
+	return useFetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=${apikey}&units=metric`)
+}
+
+function getMockData(shouldErr = false) {
+	const [data, setData] = useState<unknown>()
+	const [error, setError] = useState<Error | null>(null)
+	const [loading, setLoading] = useState<boolean>(true)
+
+	useEffect(() => {
+		setLoading(true)
+
+		setTimeout(() => {
+			if (shouldErr) {
+				setError(new Error('Something went wrong'))
+				return
+			}
+			setData(MockData)
+			console.log(MockData)
+			setLoading(false)
+		}, 1500)
+	}, [])
+
+	useEffect(() => {
+		if (error || data)
+			setLoading(false)
+	}, [data, error])
+
+	return [data, loading, error]
 }
