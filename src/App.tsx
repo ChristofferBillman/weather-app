22d744d1b7476d06620a6eec4023e3c901cd2582
ChristofferBillman <christoffer.billman@gmail.com>
2022-08-21 22:34:50 +0200

    feat: basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
 import './styles/App.css'
+import './styles/Type.css'
+
+import DataPoint from './components/DataPoint'
+
+import Thermostat from './icons/thermostat.svg'
 
 export default function App() {
 	return (
 		<div className='App'>
+			<div className='app-container'>
+				<div className='overview-container'>
+					<h1>Overview</h1>
+					<div className='datapoints-container'>
+						<DataPoint label='Temperature' data='24' icon={Thermostat} onClick={() => console.log('hello')} />
+						<DataPoint label='Temperature' data='24' icon={Thermostat} onClick={() => console.log('hello')} />
+						<DataPoint label='Temperature' data='24' icon={Thermostat} onClick={() => console.log('hello')} />
+						<DataPoint label='Temperature' data='24' icon={Thermostat} onClick={() => console.log('hello')} />
+					</div>
+				</div>
+				<div className='detail-container'>
+					<h1>Detail</h1>
+				</div>
+			</div>
 		</div>
 	)
 }
