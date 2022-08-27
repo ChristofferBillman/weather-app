1f1ba07f1fa08694aa6a56bb49131f805938750a
ChristofferBillman <christoffer.billman@gmail.com>
2022-08-28 00:01:48 +0200

    feat: lots of changes + refactoring * made lines render as part of svg instead of seperate elements * added labels for each line * various refactoring

--- src/components/Graph.tsx
+++ src/components/Graph.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react'
+import { hsl } from './Detail'
 
 interface GraphProps {
 	dataPoints: Point[]
@@ -9,8 +10,9 @@ interface GraphProps {
 interface AxisOptions {
 	boundX?: [number,number]
 	boundY?: [number,number]
+	labels?: {x: string, y: string}[]
 }
-interface Point{
+export interface Point{
 	[key: string]: number
 	x: number
 	y: number
@@ -18,14 +20,13 @@ interface Point{
 
 const HEIGHT = 256
 const WIDTH = 256 * 2
+// dataPoints / LINES is the number of lines shown.
+const LINES = 4
 
 export default function Graph({dataPoints, axisOptions, color1, color2}: GraphProps) {
 
 	const [boundX, setBoundX] = useState<[number,number]>()
 	const [boundY, setBoundY] = useState<[number,number]>()
-
-	const [numRows, setNumRows] = useState<number>(10)
-	const [numCols, setNumCols] = useState<number>(20)
 
 	const handleNoBounds = (setter: React.Dispatch<React.SetStateAction<[number, number] | undefined>>, axis: string) => {
 		const max = Math.max(...dataPoints.map((point: Point) => point[axis]))
@@ -34,8 +35,10 @@ export default function Graph({dataPoints, axisOptions, color1, color2}: GraphPr
 	}
 
 	const drawGraph = () =>{
+		const axisLabels = getAxisLabels()
+		if(axisLabels === undefined) return 
 		return (
-			<svg height={HEIGHT + 10} width={WIDTH + 10} style={{zIndex: 1}}>
+			<svg height={HEIGHT +30} width={WIDTH + 20} style={{zIndex: 1}}>
 				<defs>
 					<linearGradient id='gradient' x1="0.5" y1="1" x2="0.5" y2="0">
 						<stop offset="0%" stopColor={color1}/>
@@ -43,34 +46,98 @@ export default function Graph({dataPoints, axisOptions, color1, color2}: GraphPr
 					</linearGradient>
 				</defs>
 				<path
+					stroke={new hsl(0,0,90).toString()}
+					fill='none'
+					strokeLinecap='round'
+					strokeWidth='2'
+					d={getLines()}
+				/>
+				{axisLabels.map((labels: {x: JSX.Element, y: JSX.Element}) => <> {labels.x} {labels.y} </>)}
+				<path
 					stroke='url(#gradient)'
 					fill='none'
+					strokeLinecap='round'
 					strokeWidth='7'
 					d={getPath()}
 				/>
 			</svg>
 		)
 	}
+	const getAxisLabels = () => {
+		if(axisOptions?.labels === undefined) return
+		const labels: {x: JSX.Element, y: JSX.Element}[] = []
+		for(let i = 0; i < dataPoints.length; i++) {
+			if(i % LINES !== 0) continue
+			if(i === 0) continue
+			const currentPos: Point = {
+				x: WIDTH/dataPoints.length * i,
+				y: HEIGHT/dataPoints.length * i
+			}
+			labels[i] = {
+				x: <text
+					x={currentPos.x - 9}
+					y={HEIGHT + 20}
+					fill='gray'
+					className='graph-label'
+				>
+					{axisOptions.labels[i].x}
+				</text>,
+				y: <text
+					x={0}
+					y={currentPos.y}
+					fill='gray'
+					className='graph-label'
+				>
+					{axisOptions.labels[i].y}
+				</text>
+			}
+		}
+		return labels
+	}
+	const getLines = () => {
+		const vLine = 'v ' + HEIGHT
+		const hLine = 'h ' + WIDTH
+		let pathStringX = 'M0 0'
+		let pathStringY = 'M0 0'
+		for(let i = 0; i < dataPoints.length; i++) {
+			if(i % LINES !== 0) continue
+			if(i === 0) continue
+			const currentPos: Point = {
+				x: WIDTH/dataPoints.length * i,
+				y: HEIGHT/dataPoints.length * i
+			}
+			pathStringX += `M${currentPos.x} 0 ${vLine}`
+			pathStringY += `M0 ${currentPos.y} ${hLine}` 
+		}
+		return pathStringY +' '+ pathStringX
+	}
+
 	const getPath = () =>{
 		if(boundX === undefined || boundY === undefined) return
 		let pathString = ''
 		for(let i = 0; i < dataPoints.length; i++){
 			const pixelPos = coordToPx(dataPoints[i])
-			const cOne = coordToPx({x: dataPoints[i].x - 0.2, y: dataPoints[i].y})
-			const cTwo = coordToPx({x: dataPoints[i].x - 0.2, y: dataPoints[i].y})
+			const cOne = coordToPx({x: dataPoints[i].x, y: dataPoints[i].y})
+			const cTwo = coordToPx({x: dataPoints[i].x, y: dataPoints[i].y})
 			if(i === 0) {
-				pathString += `M0 ${HEIGHT} C${cOne.x} ${cOne.y} ${cTwo.x} ${cTwo.y} ${pixelPos.x} ${pixelPos.y} `
+				pathString += `M${0} ${HEIGHT} C${cOne.x},${cOne.y} ${cTwo.x},${cTwo.y} ${pixelPos.x},${pixelPos.y} `
 				continue
 			}
 			const lastPixelPos = coordToPx(dataPoints[i-1])
 
-			pathString+= `M${lastPixelPos.x} ${lastPixelPos.y} C${cOne.x} ${cOne.y} ${cTwo.x} ${cTwo.y} ${pixelPos.x} ${pixelPos.y} `
+			pathString+= `M${lastPixelPos.x},${lastPixelPos.y} C${cOne.x},${cOne.y} ${cTwo.x},${cTwo.y} ${pixelPos.x},${pixelPos.y} `
 		}
 		return pathString
 	}
 	const coordToPx = (p: Point): Point => {
 		if(boundX === undefined || boundY === undefined) throw new Error('coordToPx was called before boundX or boundY was defined.')
-		return {x: p.x * WIDTH / boundX[1], y: HEIGHT - (p.y * HEIGHT / boundY[1])}
+		// Convert coordinate step to pixels.
+		const dx = WIDTH / Math.abs(boundX[0] - boundX[1])
+		const dy = HEIGHT / Math.abs(boundY[0]-boundY[1])
+		// Offset beginning of graph to 0 when coordinates are negative.
+		const nOffsetX = Math.abs(boundX[0])*dx
+		const nOffsetY = Math.abs(boundY[0])*dy
+		return {x: p.x * dx + nOffsetX, y: HEIGHT - (p.y * dy) + nOffsetY}
 	}
 
 	useEffect(() => {
@@ -85,35 +152,9 @@ export default function Graph({dataPoints, axisOptions, color1, color2}: GraphPr
 		else setBoundY(axisOptions.boundY)
 	},[])
 
-	const renderGrid = (): JSX.Element => {
-		return (
-			<>
-				{[...Array(numCols)].map((e, i) => {
-					if(i !== 0) return <Line key={i} direction='horizontal' offset={WIDTH/numCols * i}/>
-				})}
-				{[...Array(numRows)].map((e, i) => {
-					if(i !== 0) return <Line key={i} direction='vertical' offset={HEIGHT/numRows * i}/>
-				})}
-			</>
-		)
-	}
-
 	return (
 		<div className='graph-container' style={{width: WIDTH + 'px', height: HEIGHT + 'px'}}>
-			{renderGrid()}
 			{drawGraph()}
 		</div>
-	)
-}
-
-interface LineProps{
-	direction: 'vertical' | 'horizontal'
-	offset: number
-}
-function Line({direction, offset}: LineProps): JSX.Element{
-	if(direction === 'vertical')
-		return <div className='line' style={{width: WIDTH + 'px', height: '2px', top: offset + 'px'}}/>
-	else return (
-		<div className='line' style={{height: HEIGHT + 'px', width: '2px',left: offset + 'px'}}/>
 	)
 }
