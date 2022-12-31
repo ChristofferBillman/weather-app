a3cbdd5ad1a0ef7908fe0b805aa07aab23f551a2
ChristofferBillman <christoffer.billman@gmail.com>
2022-12-31 15:12:32 +0100

    refactor: Graph is now a lot easier to understand and a lot shorter. Among the things that have been improved is removal of unnecessary effects.

--- src/components/Graph.tsx
+++ src/components/Graph.tsx
@@ -1,5 +1,4 @@
-import { Fragment, useEffect, useState } from 'react'
-import { hsl } from './Detail'
+import Point from '../types/Point'
 
 interface GraphProps {
 	dataPoints: Point[]
@@ -10,36 +9,45 @@ interface GraphProps {
 interface AxisOptions {
 	boundX?: [number,number]
 	boundY?: [number,number]
-	labels?: {x: string, y: string}[]
-}
-export interface Point{
-	[key: string]: number
-	x: number
-	y: number
+	xLabels?: [number, string][]
+	yLabels?: [number, string][]
 }
 
-// TODO: Labels are in wrong order.
-//       Labels are wrong when boundY is set explicitly.
-//       Negative values are not reflected in graph. Min is 0 apperently...
+// TODO: Labels are in wrong order. FIXED.
+//       Labels are wrong when boundY is set explicitly. FIXED.
+//       Negative values are not reflected in graph. Min is 0 apperently. FIXED.
+//       Rework this shit so x and y labels can be independent of each other. FIXED.
+//       Auto generate labels when omitted.
+//       Graph shows up outside svg when above a certain value. > 6 for mock temp data.
 
 const HEIGHT = 256 + 30
 const WIDTH = HEIGHT * 2
 // dataPoints / LINES is the number of lines shown.
-const LINES = 4
 
 export default function Graph({dataPoints, axisOptions, color1, color2}: GraphProps) {
 
-	const [boundX, setBoundX] = useState<[number,number]>()
-	const [boundY, setBoundY] = useState<[number,number]>()
+	let boundX = axisOptions?.boundX
+	let boundY = axisOptions?.boundY
 
-	const handleNoBounds = (setter: React.Dispatch<React.SetStateAction<[number, number] | undefined>>, axis: string) => {
-		const max = Math.max(...dataPoints.map((point: Point) => point[axis]))
-		const min = Math.min(...dataPoints.map((point: Point) => point[axis]))
-		setter([min,max])
+	// Make the graph bounds the max and min values in the data.
+	const generateBounds = (data: number[]): [number, number] => {
+		const max = Math.max(...data)
+		const min = Math.min(...data)
+		return [min,max]
 	}
+	// If boundX is not specified.
+	if(boundX === undefined) boundX = generateBounds(dataPoints.map(p => p.x))
+	// If boundY is not specified.
+	if(boundY === undefined) boundY = generateBounds(dataPoints.map(p => p.y))
 
+	/**
+	 * Draws SVG for the graph.
+	 * @returns The svg with drawn graph.
+	 */
 	const drawGraph = () =>{
-		const axisLabels = getAxisLabels()
+		const axisLabels = getLabelElements(axisOptions?.xLabels, axisOptions?.yLabels)
+		const lines = getLines(axisOptions?.xLabels, axisOptions?.yLabels)
+
 		return (
 			<svg height={HEIGHT} width={WIDTH} style={{paddingBottom: '24px'}}>
 				<defs>
@@ -48,16 +56,8 @@ export default function Graph({dataPoints, axisOptions, color1, color2}: GraphPr
 						<stop offset="100%" stopColor={color2}/>
 					</linearGradient>
 				</defs>
-				<path
-					stroke={new hsl(0,0,90).toString()}
-					fill='none'
-					strokeLinecap='round'
-					strokeWidth='2'
-					d={getLines()}
-				/>
-				{axisLabels.map(((labels: {x: JSX.Element, y: JSX.Element}, index: number) => {
-					return <Fragment key={index}> {labels.x} {labels.y} </Fragment>
-				}))}
+				{lines}
+				{axisLabels}
 				<path
 					stroke='url(#gradient)'
 					fill='none'
@@ -68,58 +68,112 @@ export default function Graph({dataPoints, axisOptions, color1, color2}: GraphPr
 			</svg>
 		)
 	}
-	const getAxisLabels = () => {
-		if(axisOptions?.labels === undefined) return
-		const labels: {x: JSX.Element, y: JSX.Element}[] = []
-		for(let i = 0; i < dataPoints.length; i++) {
-			if(i % LINES !== 0) continue
-			if(i === 0) continue
-			const currentPos: Point = {
-				x: WIDTH/dataPoints.length * i,
-				y: HEIGHT/dataPoints.length * i
-			}
-			labels[i] = {
-				x: <text
-					x={currentPos.x - 9}
-					y={HEIGHT}
-					fill='gray'
-					className='graph-label'
-				>
-					{axisOptions.labels[i].x}
-				</text>,
-				y: <text
-					x={0}
-					y={currentPos.y - 10}
-					fill='gray'
-					className='graph-label'
-				>
-					{axisOptions.labels[i].y}
-				</text>
-			}
+
+	/**
+	 * Generates the text nodes that contain the labels.
+	 * @param xLabels The placement along the X-axis and the label text.
+	 * @param yLabels The placement along the Y-axis and the label text.
+	 * @returns Text nodes with the labels with placement set.
+	 */
+	const getLabelElements = (
+		xLabels: [number, string][] | undefined,
+		yLabels: [number, string][] | undefined):
+		JSX.Element[]  => {
+
+		// Set default labels if they weren't provided. Not done yet.
+		if(xLabels == undefined) {
+			xLabels = [[0, '0']]
 		}
-		return labels
+		if(yLabels == undefined) {
+			yLabels = [[0, '0']]
+		}
+
+		const xLabelElements = xLabels.map((label: [number, string]) => (
+			<text
+				key={label[1]}
+				x={xToPx(label[0])}
+				y={HEIGHT}
+				fill='gray'
+				className='graph-label'
+			>
+				{label[1]}
+			</text>
+		))
+
+		const yLabelElements = yLabels.map((label: [number, string]) => (
+			<text
+				key={label[1]}
+				x={0}
+				y={yToPx(label[0]) + 15}
+				fill='gray'
+				className='graph-label'
+			>
+				{label[1]}
+			</text>
+		))
+
+		// Remove first and last labels. Only for aestetic reasons.
+		yLabelElements.shift()
+		xLabelElements.shift()
+		yLabelElements.pop()
+		xLabelElements.pop()
+
+		return xLabelElements.concat(yLabelElements)
 	}
-	const getLines = () => {
-		const vLine = 'v ' + HEIGHT
-		const hLine = 'h ' + (WIDTH-25)
-		let pathStringX = 'M0 0'
-		let pathStringY = 'M0 0'
-		for(let i = 0; i < dataPoints.length; i++) {
-			if(i % LINES !== 0) continue
-			if(i === 0) continue
-			const currentPos: Point = {
-				x: WIDTH/dataPoints.length * i,
-				y: HEIGHT/dataPoints.length * i
-			}
-			pathStringX += `M${currentPos.x} 0 ${vLine}`
-			pathStringY += `M25 ${currentPos.y - 5} ${hLine}`
+	/**
+	 * Get
+	 * @param xLabels The labels provided from props.
+	 * 				  Tells getLines where to place them along the X-axis.
+	 * @param yLabels The labels provided from props.
+	 * 				  Tells getLines where to place them along the Y-axis.
+	 * @returns Line nodes with placement set.
+	 */
+	const getLines = (
+		xLabels: [number, string][] | undefined,
+		yLabels: [number, string][] | undefined) => {
+
+		// Set default labels if they weren't provided.
+		if(xLabels == undefined) {
+			xLabels = [[0, '0']]
 		}
-		return pathStringY +' '+ pathStringX
+		if(yLabels == undefined) {
+			yLabels = [[0, '0']]
+		}
+
+		const xLines= xLabels.map((label: [number, string]) => (
+			<line
+				key={label[1]}
+				x1={xToPx(label[0])}
+				y1={0}
+				x2={xToPx(label[0])}
+				y2={HEIGHT}
+				className='graph-line'
+			/>
+		))
+
+		const yLines = yLabels.map((label: [number, string]) => (
+			<line
+				key={label[1]}
+				x1={0}
+				y1={yToPx(label[0])}
+				x2={WIDTH}
+				y2={yToPx(label[0])}
+				className='graph-line'
+			/>
+		))
+		
+		// Remove first and last lines. Only for aestetic reasons.
+		yLines.shift()
+		xLines.shift()
+		yLines.pop()
+		xLines.pop()
+
+		return xLines.concat(yLines)
 	}
 
 	// This sucked to code...
-	const getPath = () => {
-		if(boundX === undefined || boundY === undefined) return
+	const getPath = (): string => {
+		if(boundX === undefined || boundY === undefined) return ''
 
 		let pathString = `M${0} ${HEIGHT}`
 
@@ -198,27 +252,26 @@ export default function Graph({dataPoints, axisOptions, color1, color2}: GraphPr
 		}
 	}
 	const coordToPx = (p: Point): Point => {
-		if(boundX === undefined || boundY === undefined) throw new Error('coordToPx was called before boundX or boundY was defined.')
+		return new Point(xToPx(p.x), yToPx(p.y))
+	}
+	const xToPx = (x: number): number => {
+		if(boundX === undefined) throw new Error('xToPx was called before boundX was defined.')
 		// Convert coordinate step to pixels.
 		const dx = WIDTH / Math.abs(boundX[0] - boundX[1])
-		const dy = HEIGHT / Math.abs(boundY[0]-boundY[1])
-		// Offset beginning of graph to 0 when coordinates are negative.
+		// Offset beginning of graph to 0 when coordinates are negative
 		const nOffsetX = Math.abs(boundX[0])*dx
-		const nOffsetY = Math.abs(boundY[0])*dy
-		return {x: p.x * dx + nOffsetX, y: HEIGHT - (p.y * dy) + nOffsetY}
+		// This calculation is wrong, at least for Y. Negative points go outside.
+		return x * dx + nOffsetX
 	}
-
-	useEffect(() => {
-		if(axisOptions === undefined){
-			handleNoBounds(setBoundX, 'x')
-			handleNoBounds(setBoundY, 'y')
-			return
-		}
-		if(axisOptions.boundX === undefined) handleNoBounds(setBoundX, 'x')
-		else setBoundX(axisOptions.boundX)
-		if(axisOptions.boundY === undefined) handleNoBounds(setBoundY, 'y')
-		else setBoundY(axisOptions.boundY)
-	},[])
+	const yToPx = (y: number): number => {
+		if(boundY === undefined) throw new Error('yToPx was called before boundY was defined.')
+		// Convert coordinate step to pixels.
+		const dy = HEIGHT / Math.abs(boundY[0] - boundY[1])
+		// Offset beginning of graph to 0 when coordinates are negative.
+		const nOffsetY = Math.abs(boundY[0])*dy
+		// This calculation is wrong, at least for Y. Negative points go outside.
+		return HEIGHT - ((y * dy) + nOffsetY)
+	}
 
 	return (
 		<div className='graph-container' style={{width: WIDTH + 'px', height: HEIGHT + 'px'}}>
